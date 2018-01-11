cc17c8b887fddd463dcf2263584fc247a35de796
Nathan Alberg <alberg.nate@gmail.com>
2018-01-11 12:14:51 -0600

    changed config check from falsy to a void 0 undefined check.

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ nconf.argv().env().file({file: __dirname + '/../../config.json'});
 // will get the value or throw an error if one doesnt exist
 nconf.vet = function(key) {
   var val = nconf.get(key);
-  if (!val) {
+  if (val === void 0) {
     throw new Error('Config [' + key + '] is invalid.');
   }
   return val;
-}
+};
 
 // will get or init the value
 nconf.git = function(key, value) {
   var val = nconf.get(key);
-  if (!val) {
+  if (val === void 0) {
     nconf.set(key, value);
     return value;
   }
   return val;
-}
+};
 
 module.exports = nconf;
